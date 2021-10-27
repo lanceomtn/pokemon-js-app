9197159bf0c952f443e556873f243da11a031a81
lanceomtn <91907166+lanceomtn@users.noreply.github.com>
2021-10-27 12:37:35 -0600

    added variable list
    Created variable list with 4 poke's

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'Steak or mexican, or both';
-document.write(favoriteFood);
+let pokemonList = [
+  {name:'Bulbasar', height:7, type: ['grass', 'poison']}
+  {name:'Squirtle', height:2, type: ['water']}
+  {name:'Beedrill', height:3, type: ['bug', 'poison']}
+  {name:'Geodude', height:4, type: ['rock']}
+]
