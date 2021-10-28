3f60d028ac0dd9d573a99599f3e5c9d8743eb7e4
lanceomtn <91907166+lanceomtn@users.noreply.github.com>
2021-10-28 11:33:42 -0600

    added loop and conditional
    Initial for loop, if conditional and document write to print pokemons, their height and conditional message

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
+//initial array with pokemon data
 let pokemonList = [
-  {name:'Bulbasar', height:7, type: ['grass', 'poison']}
-  {name:'Squirtle', height:2, type: ['water']}
-  {name:'Beedrill', height:3, type: ['bug', 'poison']}
-  {name:'Geodude', height:4, type: ['rock']}
+  {name:'Bulbasar', height: .5, type: ['grass', 'poison']},
+  {name:'Squirtle', height: 1, type: ['water']},
+  {name:'Beedrill', height: 1.5, type: ['bug', 'poison']},
+  {name:'Geodude', height: 4, type: ['rock']}
 ]
+
+//list pokepeoples by name and height
+document.write('<ul>')
+for (let i = 0; i < pokemonList.length; i++)  {
+  document.write( `<li> ${pokemonList[i].name} - height: ${pokemonList[i].height} </li>` );
+//evaluate if pokemon excedes certain height and write message if true
+  if (pokemonList[i].height > 2.0){ document.write(" - Wow that is a tall pokemon!");
+  }
+}
+document.write('</ul>')
