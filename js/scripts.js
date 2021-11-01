434f679812ef692c0cc4ba302081b329a8a5c7b7
lanceomtn <91907166+lanceomtn@users.noreply.github.com>
2021-11-01 14:54:34 -0600

    added li and button elements (DOM manipulation)
    added li and button elements to replace document write
    
    added event listener for button click and record clicks to console

--- js/scripts.js
+++ js/scripts.js
@@ -8,29 +8,51 @@ let pokemonRepository = (function () {
   {name:'Geodude', height: 4, type: ['rock']}
 ];
 
-//add pokemon to the pokemon list array
-  function add(pokemon) {
-		pokemonList.push(pokemon);
-	}
+//IIFE returning object with 2 keys (add and getAll - there could be more)
+function add(pokemon) {
+    pokemonList.push(pokemon);
+}
 
-//getAll pokemon data from the list array
-  function getAll() {
-		return pokemonList;
-	}
+function getAll () {
+    return pokemonList;
+}
 
-//return is add + getAll??
-  return {
-		add: add,
-		getAll: getAll()
-	};
-})();
+//create ul on html page to display pokemon
+function addListItem (pokemon) {
+	//assigns pokemon-list class for index to pokemonList variable
+	let pokemonList = document.querySelector('.pokemon-list');
+	//create li within the ul (this is the child?)
+	let listPokemon = document.createElement('li');
+	//add button element within li
+	let button = document.createElement('button');
+	//put text (in this case pokemon name) inside button
+	button.innerText = pokemon.name;
+	//refer button to css .button-class for styling
+	button.classList.add('button-class')
+	//call listPokemon and append button within li
+	listPokemon.appendChild(button);
+	//append li within the ul (the parent element)
+	pokemonList.appendChild(listPokemon);
+	//add event listner for click on button defined above
+	button.addEventListener('click', function() {
+    showDetails(pokemon)
+  });
+}
 
-//create loop that iterates over array data
-pokemonRepository.getAll.forEach(function(pokemon) {
-//evaluate pokemon height vs variable if condition met print one message
-  if(pokemon.height > 2.0 ){
-   document.write('<li>'+ pokemon.name + ' - height: ' +  pokemon.height + ' Wow that is a tall Pokemon!  </li>');
-  }
-//if height variable conditon not met print this message
-  else document.write('<li>'+ pokemon.name + ' - height: ' +  pokemon.height +'  </li>');
-}	)
+function showDetails(pokemon) {
+  console.log(pokemon);
+}
+
+//calling the above functions in the return
+return {
+  getAll: getAll,
+  add: add,
+  addListItem: addListItem,
+	showDetails: showDetails
+};
+//end of the IIFE
+} )();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+});
